2883ba510a3ce2b170be51a63a1d411e0306664a
Rahul Kumar <76434100+iamrahul25@users.noreply.github.com>
2022-08-29 13:19:25 +0530

    Create 5_Rotate_array_by_k_places.cpp
    https://leetcode.com/problems/rotate-array/

---
+++ Array/Easy/5_Rotate_array_by_k_places.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    
+    //Method 2: Space O(1)----------------Optimised
+    void rotate(vector<int>& nums, int k){
+        
+        int n = nums.size();
+        
+        //Important Condition.
+        k = k%n;
+        
+        //Reverse 0 to k-1 element
+        reverse(nums.begin(), nums.begin()+n-k);
+        
+        //Reverse k to n-1 element
+        reverse(nums.end()-k, nums.end());
+        
+        //Reverse 0 to n-1 element (Whole Array)
+        reverse(nums.begin(), nums.end());
+        
+    }
+    
+    
+    //Method 1: Space O(N)----------------
+//     void rotate(vector<int>& nums, int k){
+        
+//         int n = nums.size();
+        
+//         k = k%n;
+        
+//         vector<int> vec(n);
+        
+//         for(int i=0; i<n; i++){
+//             vec[i] = nums[(i+n-k)%n];
+//         }
+        
+//         //Updating original Vector:
+//         for(int i=0; i<n; i++){
+//             nums[i] = vec[i];
+//         }
+//     }
+    
+};
