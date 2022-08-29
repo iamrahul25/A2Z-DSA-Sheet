31dbc941275fad92a7540085201765b53352c799
Rahul Kumar <76434100+iamrahul25@users.noreply.github.com>
2022-08-29 12:29:10 +0530

    Create 4_Remove_duplicates_from_sorted_array_without_using_extra_space.cpp
    https://leetcode.com/problems/remove-duplicates-from-sorted-array/

---
+++ Array/Easy/4_Remove_duplicates_from_sorted_array_without_using_extra_space.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    
+    
+    //-----------Approach 1: Two Pointer ------------------
+    int removeDuplicates(vector<int>& nums) {
+        
+        int p1 = 0; 
+        int p2 = 1;
+        int count = 1;
+        
+        while(p2<nums.size()){
+            if(nums[p1]==nums[p2]){
+                p2++;
+            }
+            else{
+                p1++;
+                nums[p1] = nums[p2];
+                count++;
+            }
+        }
+        
+        return count;
+        
+    }
+    
+};
