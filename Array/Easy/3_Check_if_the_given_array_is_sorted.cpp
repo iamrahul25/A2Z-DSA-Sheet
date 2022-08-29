dc569070841b3233d3f0da74bed12cf156384c32
Rahul Kumar <76434100+iamrahul25@users.noreply.github.com>
2022-08-29 12:03:42 +0530

    Create 3_Check_if_the_given_array_is_sorted.cpp
    https://practice.geeksforgeeks.org/problems/check-if-an-array-is-sorted0701/1

---
+++ Array/Easy/3_Check_if_the_given_array_is_sorted.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    bool arraySortedOrNot(int arr[], int n) {
+        
+        for(int i=0; i<n-1; i++){
+            
+            if(arr[i]<=arr[i+1]) continue;
+            
+            else{
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
