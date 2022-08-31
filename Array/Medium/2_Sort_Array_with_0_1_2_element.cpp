fb5921111f910df41523484ca95be62cd29e3072
Rahul Kumar <76434100+iamrahul25@users.noreply.github.com>
2022-08-31 12:16:17 +0530

    Update 2_Sort_Array_with_0_1_2_element.cpp

--- Array/Medium/2_Sort_Array_with_0_1_2_element.cpp
+++ Array/Medium/2_Sort_Array_with_0_1_2_element.cpp
@@ -22,18 +22,18 @@ public:
         
     }
     
-    Approach 2: Using Swapping
-    void sortColors(vector<int>& nums){
-        int n = nums.size();
-        int j = 0, k = n-1;
+    //Approach 2: Using Swapping
+//     void sortColors(vector<int>& nums){
+//         int n = nums.size();
+//         int j = 0, k = n-1;
         
-        for (int i=0; i <= k; i++) {
-            if (nums[i] == 0)
-                swap(nums[i], nums[j++]);
-            else if (nums[i] == 2)
-                swap(nums[i--], nums[k--]);
-        }
-    }
+//         for (int i=0; i <= k; i++) {
+//             if (nums[i] == 0)
+//                 swap(nums[i], nums[j++]);
+//             else if (nums[i] == 2)
+//                 swap(nums[i--], nums[k--]);
+//         }
+//     }
     
     
     
