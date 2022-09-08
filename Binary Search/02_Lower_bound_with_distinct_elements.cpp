2621a5dbee7ea70185d1678c6a4886e55935b0a9
Rahul Kumar <76434100+iamrahul25@users.noreply.github.com>
2022-09-08 21:58:28 +0530

    Create 02_Lower_bound_with_distinct_elements.cpp
    https://practice.geeksforgeeks.org/problems/floor-in-a-sorted-array-1587115620/1

---
+++ Binary Search/02_Lower_bound_with_distinct_elements.cpp
@@ -0,0 +1,31 @@
+class Solution{
+  public:
+    //Implement => Lower Bound - with distinct elements. 
+    int findFloor(vector<long long> v, long long n, long long x){
+        
+        long long low = 0;
+        long long high = n-1;
+        long long mid = -1;
+        
+        //If x greater than last element. 
+        if(x>v[n-1]){
+            return n-1;
+        }
+        
+        while(low<=high){
+            mid = (low+high)/2;
+            
+            if(v[mid]==x){
+                return mid;
+            }
+            else if(v[mid]<x){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        
+        return mid-1;
+    }
+};
